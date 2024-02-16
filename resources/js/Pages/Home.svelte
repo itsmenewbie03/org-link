860c3731574e26bcb96031bbe26f19c1402eeb74
itsmenewbie03 <2101102516@student.buksu.edu.ph>
2024-02-16 22:56:57 +0800

    feat: setup inertia.js to use svelte
    This commit adds inertia.js and complete the setup to make it work.
    Currently the root route will return an a page that is built with
    svelte.

---
+++ resources/js/Pages/Home.svelte
@@ -0,0 +1,9 @@
+<script>
+    export let user;
+</script>
+
+<svelte:head>
+    <title>Welcome, {user}</title>
+</svelte:head>
+<h1>Welcome</h1>
+<p>Hello {user}, welcome to your first Inertia app!</p>
