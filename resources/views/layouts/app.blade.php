9d220a4d37ad7956497900b9b6c7dae4b8d9c50e
itsmenewbie03 <2101102516@student.buksu.edu.ph>
2024-04-11 14:10:25 +0800

    fix: remove setting nav item

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -53,10 +53,6 @@
 
                 <x-mary-menu-item title="Dashboard" icon="o-computer-desktop" link="{{ route('dashboard') }}" />
                 <x-mary-menu-item title="Tenants" icon="o-users" link="{{ route('tenants.index') }}" />
-                <!-- <x-mary-menu-sub title="Settings" icon="o-cog-6-tooth"> -->
-                <!--     <x-mary-menu-item title="Wifi" icon="o-wifi" link="####" /> -->
-                <!--     <x-mary-menu-item title="Archives" icon="o-archive-box" link="####" /> -->
-                <!-- </x-mary-menu-sub> -->
             </x-mary-menu>
         </x-slot:sidebar>
 
