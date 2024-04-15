6bdb052cfe9a840a3006f430069a150e89f1c576
itsmenewbie03 <2101102516@student.buksu.edu.ph>
2024-04-15 22:08:02 +0800

    feat: add org name and plan
    This add support for the name and plan of the tenant.

--- app/Models/Tenant.php
+++ app/Models/Tenant.php
@@ -11,15 +11,17 @@ class Tenant extends BaseTenant implements TenantWithDatabase
 {
     use HasDatabase;
     use HasDomains;
-    protected $fillable = ['id','name', 'email'];
+    protected $fillable = ['id','organization_name','name','email','plan'];
 
     // NOTE: you need to read the docs carefully
     public static function getCustomColumns(): array
     {
         return [
             'id',
+            'organization_name',
             'name',
-            'email'
+            'email',
+            'plan',
         ];
     }
 }
