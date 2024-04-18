f15ce96852046994b447a5541787c941b465c992
itsmenewbie03 <2101102516@student.buksu.edu.ph>
2024-04-18 11:00:27 +0800

    feat: add profile settings button
    Add a button that takes the user to the `profile` route that comes with
    laravel breeze.

---
+++ resources/views/livewire/username-quick-actions.blade.php
@@ -0,0 +1,22 @@
+<?php
+
+use Livewire\Volt\Component;
+
+new class extends Component {
+    public function logout()
+    {
+        auth()->logout();
+        return redirect()->route('login');
+    }
+    public function settings()
+    {
+        return redirect()->route('profile');
+    }
+}; ?>
+
+<div>
+    <x-mary-button icon="o-cog-6-tooth" wire:click="settings" class="btn-circle btn-ghost btn-xs"
+        tooltip-left="Profile Settings" />
+    <x-mary-button icon="o-arrow-right-on-rectangle" wire:click="logout" class="btn-circle btn-ghost btn-xs"
+        tooltip-left="Logout" />
+</div>
