b948acdb85afe03f756dec0566f0d91631b5d8a4
itsmenewbie03 <2101102516@student.buksu.edu.ph>
2024-04-25 22:41:00 +0800

    feat: add smart db hack
    This utilizes the fact that a tenant is tied with a subdomain in the
    format of `tenant_id.domain` so we can use that to get the tenant_id and
    query the central db to get the full information of tenant for
    instances that we can't reach the `tenant()` helper functin.

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Request;
+use App\Models\Tenant;
 
 if (! function_exists('stupid_db_hack')) {
     function stupid_db_hack(string $db_name)
@@ -10,6 +12,17 @@ if (! function_exists('stupid_db_hack')) {
     }
 }
 
+// TODO: migrate all stupid hacks to the smarter one
+if (! function_exists('smart_db_hack')) {
+    function smart_db_hack()
+    {
+        $domain = Request::getHost();
+        $db_name = explode('.', $domain)[0];
+        $tenant = Tenant::find($db_name);
+        config(['database.connections.new.database' => $tenant->tenancy_db_name]);
+        DB::setDefaultConnection('new');
+    }
+}
 
 if (! function_exists('end_stupid_db_hack')) {
     function end_stupid_db_hack()
