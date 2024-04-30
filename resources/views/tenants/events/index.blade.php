e42b26c52aafe52a95f413a12952fb648b161d3b
itsmenewbie03 <2101102516@student.buksu.edu.ph>
2024-04-30 20:49:48 +0800

    feat: load events from database
    This implement the loading of event from the database.

--- resources/views/tenants/events/index.blade.php
+++ resources/views/tenants/events/index.blade.php
@@ -34,7 +34,32 @@
     @endphp
 
     @php
-        $users = App\Models\User::take(3)->get();
+
+        $calendar_events = [];
+        $events = App\Models\TenantEvents::all();
+
+        // NOTE: this transform the $events to match the desired input of the calendar component
+        foreach ($events as $evt) {
+            $evt->start_date = Carbon\Carbon::parse($evt->start_date);
+            $evt->end_date = Carbon\Carbon::parse($evt->end_date);
+            if ($evt->start_date->diffInDays($evt->end_date) >= 1) {
+                $evt_transformed = [
+                    'label' => $evt->name,
+                    'description' => $evt->description,
+                    'css' => '!bg-rose-200',
+                    'range' => [$evt->start_date, $evt->end_date],
+                ];
+                $calendar_events[] = $evt_transformed;
+                continue;
+            }
+            $evt_transformed = [
+                'label' => $evt->name,
+                'description' => $evt->description,
+                'css' => '!bg-purple-200',
+                'date' => $evt->start_date,
+            ];
+            $calendar_events[] = $evt_transformed;
+        }
     @endphp
 
     <div class="text-lg breadcrumbs">
@@ -45,14 +70,14 @@
     <div class="w-full flex">
         <livewire:tenant-event-quick-actions />
         <x-mary-card title="Upcoming Events" class="w-1/3 mr-6" shadow separator>
-            @foreach ($users as $user)
-                <x-mary-list-item :item="$user" />
+            @foreach ($events as $event)
+                <x-mary-list-item :item="$event" />
             @endforeach
         </x-mary-card>
 
         <x-mary-card title="Monthly Events Calendar" class="w-fit" shadow separator>
             {{-- TODO: get rid of this ugly config call --}}
-            <x-mary-calendar locale="en-PH" :events="$events" weekend-highlight :config="['settings' => ['iso8601' => false]]" />
+            <x-mary-calendar locale="en-PH" :events="$calendar_events" weekend-highlight :config="['settings' => ['iso8601' => false]]" />
         </x-mary-card>
     </div>
 @endsection
