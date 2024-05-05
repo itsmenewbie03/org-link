2112b87ce552dd61be5abfea5f9bd0c063290c5e
itsmenewbie03 <2101102516@student.buksu.edu.ph>
2024-05-05 20:53:27 +0800

    feat: add proper fields
    This adds the fields that are required in most of the attendance. This is from
    the advice of @Toshiven.

--- resources/views/livewire/tenant-attendance-start.blade.php
+++ resources/views/livewire/tenant-attendance-start.blade.php
@@ -4,8 +4,16 @@ use Livewire\Volt\Component;
 use Mary\Traits\Toast;
 
 new class extends Component {
+    public array $year_levels = [['id' => 1, 'name' => '1st Year'], ['id' => 2, 'name' => '2nd Year'], ['id' => 3, 'name' => '3rd Year'], ['id' => 4, 'name' => '4th Year'], ['id' => 5, 'name' => 'Irregular']];
+
     use Toast;
     public ?object $event = null;
+
+    // NOTE: attendance fields
+    public string $name;
+    public string $email;
+    public string $course;
+    public int $year;
 
     public function back()
     {
@@ -26,8 +34,10 @@ new class extends Component {
     </div>
     <x-mary-card title="{{ $this->event->name }} Attendance">
         <x-mary-form>
-            <x-mary-input label="Name" wire:model="tenant_name" icon="o-user" inline />
-            <x-mary-input label="E-mail" wire:model="tenant_email" icon="o-at-symbol" inline />
+            <x-mary-input label="Name" wire:model="name" icon="o-user" inline />
+            <x-mary-input label="Email" wire:model="email" icon="o-at-symbol" inline />
+            <x-mary-input label="Course" wire:model="course" icon="o-academic-cap" inline />
+            <x-mary-select label="Year" icon="o-star" :options="$year_levels" wire:model="selectedUser" inline />
             <x-slot:actions>
                 <x-mary-button label="Cancel" wire:click="back" />
                 <x-mary-button label="Save" icon="o-arrow-up-on-square" class="btn-primary"
