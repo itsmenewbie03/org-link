b38cb7218bd007a58b880e083b8fe09f056db3a4
itsmenewbie03 <2101102516@student.buksu.edu.ph>
2024-05-09 08:04:04 +0800

    wip: add multiple themes

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -16,6 +16,7 @@
                     <x-mary-button icon="o-plus" class="btn-primary" />
                 </x-slot:actions>
             </x-mary-header>
+            <x-mary-theme-toggle darkTheme="lofi" lightTheme="lofi" />
         @else
             @yield('content')
         @endif
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,57 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
+import defaultTheme from "tailwindcss/defaultTheme";
+import forms from "@tailwindcss/forms";
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-		'./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-		 './storage/framework/views/*.php',
-		 './resources/views/**/*.blade.php',
-		 "./vendor/robsontenorio/mary/src/View/Components/**/*.php"
-	],
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
+        "./vendor/robsontenorio/mary/src/View/Components/**/*.php",
+    ],
 
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ["Figtree", ...defaultTheme.fontFamily.sans],
             },
         },
     },
-
-    plugins: [
-		forms,
-		require("daisyui")
-	],
+    daisyui: {
+        themes: [
+            "light",
+            "dark",
+            "cupcake",
+            "bumblebee",
+            "emerald",
+            "corporate",
+            "synthwave",
+            "retro",
+            "cyberpunk",
+            "valentine",
+            "halloween",
+            "garden",
+            "forest",
+            "aqua",
+            "lofi",
+            "pastel",
+            "fantasy",
+            "wireframe",
+            "black",
+            "luxury",
+            "dracula",
+            "cmyk",
+            "autumn",
+            "business",
+            "acid",
+            "lemonade",
+            "night",
+            "coffee",
+            "winter",
+            "dim",
+            "nord",
+            "sunset",
+        ],
+    },
+    plugins: [forms, require("daisyui")],
 };
