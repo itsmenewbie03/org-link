319a90a400569d30aa096e27d2a6209f7023431f
itsmenewbie03 <2101102516@student.buksu.edu.ph>
2024-05-11 21:35:27 +0800

    feat: add self updater

--- routes/web.php
+++ routes/web.php
@@ -32,4 +32,27 @@ Route::get("/mail", function () {
     return new TenantWelcomeEmail(new Tenant(), "deeznuts");
 });
 
+Route::get('/update', function (\Codedge\Updater\UpdaterManager $updater) {
+
+    // Check if new version is available
+    if($updater->source()->isNewVersionAvailable()) {
+
+        // Get the current installed version
+        echo $updater->source()->getVersionInstalled();
+
+        // Get the new version available
+        $versionAvailable = $updater->source()->getVersionAvailable();
+
+        // Create a release
+        $release = $updater->source()->fetch($versionAvailable);
+
+        // Run the update process
+        $updater->source()->update($release);
+
+    } else {
+        echo "No new version available.";
+    }
+
+})->middleware(['auth']);
+
 require __DIR__.'/auth.php';
